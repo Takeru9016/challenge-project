b92f231b93303fe5b091fe1fa792426f4463afce
Takeru9016 <youthfox046@gmail.com>
2022-12-19 19:17:11 +0530

    Day 2 - Connecting the Santiy to backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 function page() {
-  return <div className="text-4xl">Pages</div>;
+  return <div className="flex justify-center items-center text-4xl">Welcome to Data Warehouse</div>;
 }
 
 export default page;
